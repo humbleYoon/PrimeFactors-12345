916f266541537bf25152fe7ca493d4650917c46d
KimYoonjae <rladbswo1124@gmail.com>
2024-05-16 17:08:30 +0900

    [feature] page 44

--- primeFactors/prime-factors.cpp
+++ primeFactors/prime-factors.cpp
@@ -5,8 +5,15 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> ret = {};
-		if (number >= 2)
-			ret.push_back(number);
+		if (number >= 2){
+			if (number == 4) {
+				ret.push_back(2);
+				ret.push_back(2);
+			}
+			else {
+				ret.push_back(number);
+			}
+		}
 		return ret;
 	}
 };
