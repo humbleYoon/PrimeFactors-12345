0bcdda0fd8b6a6018d3be86e566235fef73dba09
KimYoonjae <rladbswo1124@gmail.com>
2024-05-17 08:49:30 +0900

    [refactoring] page 57

--- primeFactors/prime-factors.cpp
+++ primeFactors/prime-factors.cpp
@@ -5,25 +5,15 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1){
+		if (number > 1) {
 			int divisor = 2;
-			if (number == 4) {
-				while (number % divisor == 0) {
-					result.push_back(divisor);
-					number /= divisor;
-				}
-			}
-			else if (number == 6) {
+			if (number == 4 || number == 6 || number == 9) {
 				for (divisor = 2; number > 1; divisor++) {
 					while (number % divisor == 0) {
 						result.push_back(divisor);
 						number /= divisor;
 					}
 				}
-			}
-			else if (number == 9) {
-				result.push_back(3);
-				result.push_back(3);
 			}
 			else {
 				result.push_back(number);
