27b50d649e940c713d7a40f4efdee5c8c754f5e2
KimYoonjae <rladbswo1124@gmail.com>
2024-05-16 16:49:10 +0900

    [feature] 33 page

--- Sample-Test1/prime-factors-test.cpp
+++ Sample-Test1/prime-factors-test.cpp
@@ -1,8 +1,17 @@
 #include "pch.h"
 #include "../primeFactors/prime-factors.cpp"
 
-using namespace std;
+// 2->[2]
+// 3->[3]
+// 4->[2, 2]
+// 6->[2, 3]
+// 8->[2, 2, 2]
+// 9->[3, 3]
+// 12->[2, 2, 3]
+// 14->[2, 7]
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
