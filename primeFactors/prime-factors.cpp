a1a57925aba078633f42eff2bef945486f0323a6
KimYoonjae <rladbswo1124@gmail.com>
2024-05-16 17:00:21 +0900

    [refactoring] page 41

--- primeFactors/prime-factors.cpp
+++ primeFactors/prime-factors.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 class PrimeFactor {
 public:
-	vector<int> of(int n) {
+	vector<int> of(int number) {
 		vector<int> ret = {};
-		if (n == 2)
-			ret.push_back(2);
-		else if (n == 3)
-			ret.push_back(3);
+		if (number == 2)
+			ret.push_back(number);
+		else if (number == 3)
+			ret.push_back(number);
 		return ret;
 	}
 };
